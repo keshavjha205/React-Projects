771286aba3e5ca64ebc9d1f98377725513addb46
keshavjha205 <keshavjha1081@gmail.com>
2025-02-24 12:43:32 +0530

    FoodHub

--- FoodHub/src/App.jsx
+++ FoodHub/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
 
-const App =() =>{
-  return(
-   <div className='min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50'>
-     <Navbar/>
-   </div>
+const App = () => {
+  return (
+    <div className='min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50'>
+      <Navbar />
+      <br />
+
+      <main className='flex-1 w-full'>
+        <div className=' w-full'>
+          <Banner />
+        </div>
+      </main>
+    </div>
   )
 }
 export default App
---
+++ FoodHub/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className=' bg-white pt-12 md:pt-24 min-h-[500px] flex items-center'>
+        
+    </div>
+  )
+}
+
+export default Banner
