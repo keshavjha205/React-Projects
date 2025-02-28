faf86072a6a9c49f99a7a2d524cbe9daf004f83c
keshavjha205 <keshavjha1081@gmail.com>
2025-02-28 22:03:00 +0530

    .......

--- onlineMandi/src/App.jsx
+++ onlineMandi/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Cards from "./component/Cards"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  function filterHandeler(){
+
+  }
 
   return (
-<div></div>
+    <div >
+      <div className="text-center text-3xl pt-3 ">
+        <h1>Online Baazar</h1>
+        <div >
+          <div className="mb-2">
+            <input type="text" placeholder="Search..." className=" rounded-full rounded-tr-none rounded-br-none border-1 outline-none pl-0.5" />
+            <button onClick={()=>{}} className="border-1 rounded-full rounded-tl-none rounded-bl-none active:scale-95 p-0.5 border-l-0 bg-sky-300 ">🔍</button>
+          </div>
+        </div>
+      </div>
+      <br />
+      <Cards />
+    </div>
   )
 }
 
