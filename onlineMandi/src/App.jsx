c03f963e314a71fc65ed9f5b9c2b2c0d96330351
keshavjha205 <keshavjha1081@gmail.com>
2025-03-01 12:34:14 +0530

    .....

--- onlineMandi/src/App.jsx
+++ onlineMandi/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
 import Cards from "./component/Cards"
+import { data } from "./utilis/data";
 
 
 function App() {
-  function filterHandeler(){
+  const [val, setVal] = useState("")
+  const [filterData, setFilterData] = useState(data)
 
+  function filterHandeler(){
+    let res = data.filter((item)=> item.title.toLocaleLowerCase().includes(val.toLocaleLowerCase()))
+    setFilterData(res)
+    
   }
 
   return (
@@ -12,13 +19,19 @@ function App() {
         <h1>Online Baazar</h1>
         <div >
           <div className="mb-2">
-            <input type="text" placeholder="Search..." className=" rounded-full rounded-tr-none rounded-br-none border-1 outline-none pl-0.5" />
-            <button onClick={()=>{}} className="border-1 rounded-full rounded-tl-none rounded-bl-none active:scale-95 p-0.5 border-l-0 bg-sky-300 ">🔍</button>
+            <input type="text" placeholder="Search..."
+             className=" rounded-full rounded-tr-none rounded-br-none border-1 outline-none pl-0.5"
+             onChange={(e) => setVal(e.target.value)} />
+
+            <button onClick={filterHandeler}
+             className="border-1 rounded-full rounded-tl-none rounded-bl-none active:scale-95 p-0.5 border-l-0 bg-sky-300 "
+             >🔍</button>
           </div>
         </div>
       </div>
       <br />
-      <Cards />
+      {filterData.length === 0 ? <h1  className="text-center font-serif text-5xl font-semibold mt-10">No Items Found</h1> : <Cards products={filterData} />}
+      
     </div>
   )
 }
